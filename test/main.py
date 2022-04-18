01ada1b09d1deda3528253cb00441abb94119087
Jaime <guzmanpjaime@gmail.com>
2022-04-18 07:30:16 -0500

    Creacion de hash.py para leer documentos pdf y aplicar un hash, se bbusca implementar este documento como funcion en main.py

--- test/main.py
+++ test/main.py
@@ -1,11 +1,19 @@
 from hashlib import sha256
 from random import randint
+import os
 
 def hashThis(r, M):
     hash=sha256();
     hash.update(str(r).encode());
     hash.update(M.encode());
     return int(hash.hexdigest(),16);
+
+def hashPDF(file, BLOCK_SIZE):
+    with open(file, 'rb') as f: # Open the file to read it's bytes
+        fb = f.read(BLOCK_SIZE) # Read from the file. Take in the amount declared above
+    while len(fb) > 0: # While there is still data being read from the file
+        hash.update(fb) # Update the hash
+        fb = f.read(BLOCK_SIZE) # Read the next block from the file
 
 
 ## Notation
@@ -22,10 +30,13 @@ x = 32991
 y = pow(g, x, q)
 
 ## Signing
-M = "This is the message"
+# M = "This is the message"
+file = "./test/test.pdf"
+BLOCK_SIZE =  os.path.getsize(file) 
 k = randint(1, q - 1)
 r = pow(g, k, q)
-e = hashThis(r, M) % q # part 1 of signature
+# e = hashThis(r, M) % q # part 1 of signature
+e = hashPDF(file, BLOCK_SIZE)
 s = (k - (x * e)) % (q-1) # part 2 of signature
 
 ## Verification
