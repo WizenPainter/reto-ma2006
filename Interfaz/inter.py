db8bf19c4ebd36b3898482341a67fb583347ac20
gerardo-delvalle <73604339+gerardo-delvalle@users.noreply.github.com>
2022-04-20 20:56:57 -0500

    una nueva estructura del gui

--- Interfaz/inter.py
+++ Interfaz/inter.py
@@ -1,14 +1,56 @@
 import PySimpleGUI as sg
-sg.theme("DarkTeal2")
-layout = [[sg.T("")], [sg.Text("Choose a file: "), sg.Input(key="-IN2-" ,change_submits=True), sg.FileBrowse(key="-IN-")],[sg.Button("Submit"), sg.Button('Cancel')]]
+import create_keypair as ckp
+import schnorr_lib as sl
+import os
 
-###Building Window
-window = sg.Window('My File Browser', layout, size=(600,150))
-    
-while True:
-    event, values = window.read()
-    print(values["-IN2-"])
-    if event == sg.WIN_CLOSED or event=="Exit" or event=="Cancel":
-        break
-    elif event == "Submit":
-        print(values["-IN-"])
+def hashPDF(file, BLOCK_SIZE):
+    # hash=sha256()
+    with open(file, 'rb') as f: # Open the file to read it's bytes
+        fb = f.read(BLOCK_SIZE) # Read from the file. Take in the amount declared above
+        M = sl.sha256(fb)
+    return M
+
+def make_win1():
+
+    layout = [[sg.Text('Escribir mail del destinatario')],
+        [sg.Text('Mail: '), sg.InputText()],#[sg.T("")], 
+        [sg.Text("Elegir el archivo: "), sg.Input(change_submits=True), sg.FileBrowse(key="-Archivo-")],
+        [sg.Button("Generar firmas Schnorr"),sg.Button("Verificar Firma"), sg.Button('Salir')]]
+    return sg.Window('Programa de Firmas Schnorr', layout, finalize=True)
+
+def make_win2():
+
+    layout = [[sg.Text('The second window')],
+              [sg.Input(key='-IN-', enable_events=True)],
+              [sg.Text(size=(25,1), k='-OUTPUT-')],
+              [sg.Button('Erase'), sg.Button('Popup'), sg.Button('Exit')]]
+    return sg.Window('Second Window', layout, finalize=True)
+
+
+window1, window2 = make_win1(), None        # start off with 1 window open
+while True:             # Event Loop
+    window, event, values = sg.read_all_windows()
+    if event == sg.WIN_CLOSED or event == 'Salir':
+        window.close()
+        if window == window2:       # if closing win 2, mark as closed
+            window2 = None
+        elif window == window1:     # if closing win 1, exit program
+            break
+    elif event == "Generar firmas Schnorr":
+        size = os.path.getsize(values["-Archivo-"]) 
+        user = ckp.create_keypair(1)["users"]
+        M = hashPDF(values["-Archivo-"], size)
+        sig = sl.schnorr_sign(M, user[0]["privateKey"])
+        PubPublicKey = user[0]["publicKey"]
+        Signature = sig.hex()
+
+        sg.Popup("Clave Pública: ", PubPublicKey, "Firma: ", Signature)
+
+    elif event == 'Verificar Firma' and not window2:
+        window2 = make_win2()
+    elif event == '-IN-':
+        window['-OUTPUT-'].update(f'You enetered {values["-IN-"]}')
+    elif event == 'Erase':
+        window['-OUTPUT-'].update('')
+        window['-IN-'].update('')
+window.close()
--- schnorr-sig/interfaz.py
+++ schnorr-sig/interfaz.py
@@ -1,24 +1,79 @@
-# img_viewer.py
-
-# hello_psg.py
-
 import PySimpleGUI as sg
+import create_keypair as ckp
+import schnorr_lib as sl
+import os
 
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Escribir mail del destinatario')],
-            [sg.Text('Mail: '), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')],
-            [sg.FileBrowse(key="-IN-")] ]
+def hashPDF(file, BLOCK_SIZE):
+    # hash=sha256()
+    with open(file, 'rb') as f: # Open the file to read it's bytes
+        fb = f.read(BLOCK_SIZE) # Read from the file. Take in the amount declared above
+        M = sl.sha256(fb)
+    return M
 
-# Create the Window
-window = sg.Window('Verificación Schnorr', layout)
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-        break
-    print('You entered ', values[0])
-    print(values["-IN-"])
+def make_win1():
+
+    layout = [[sg.Text('Escribir mail del destinatario')],
+        [sg.Text('Mail: '), sg.InputText()],#[sg.T("")], 
+        [sg.Text("Elegir el archivo: "), sg.Input(change_submits=True), sg.FileBrowse(key="-Archivo-")],
+        [sg.Button("Generar firmas Schnorr"),sg.Button("Verificar Firma"), sg.Button('Salir')]]
+
+    return sg.Window('Programa de Firmas Schnorr', layout, finalize=True)
+
+def make_win2():
+
+    layout = [[sg.Text('Inserta la Clave Pública (o agregada si se utilizó el esquema MuSig):')],
+              [sg.Input(key='-Clave-', enable_events=True)],
+              [sg.Text('Inserta la Firma Generada:')],
+              [sg.Input(key='-Firma-', enable_events=True)],
+              [sg.Button("Verificar"), sg.Button("Salir")]]
+
+    return sg.Window('Second Window', layout, finalize=True)
+
+
+window1, window2 = make_win1(), None        # start off with 1 window open
+while True:           # Event Loop
+    
+    window, event, values = sg.read_all_windows()
+
+    #path = values['-Archivo-']
+    #size = os.path.getsize(path)
+    #M = hashPDF(path, size) 
+    if event == sg.WIN_CLOSED or event == 'Salir':
+        window.close()
+        if window == window2:       # if closing win 2, mark as closed
+            window2 = None
+        elif window == window1:     # if closing win 1, exit program
+            break
+    elif event == "Generar firmas Schnorr":
+        size = os.path.getsize(values["-Archivo-"]) 
+        user = ckp.create_keypair(1)["users"]
+        M = hashPDF(values["-Archivo-"], size)
+        sig = sl.schnorr_sign(M, user[0]["privateKey"])
+        PubPublicKey = user[0]["publicKey"]
+        Signature = sig.hex()
+
+        sg.Popup("Clave Pública: ", PubPublicKey, "Firma: ", Signature)
+
+    elif event == 'Verificar Firma' and not window2:
+        window2 = make_win2()
+
+
+    elif event == "Verificar":
+        print(values['-Archivo-'])
+        size = os.path.getsize(values['-Archivo-']) 
+        M = hashPDF(values['-Archivo-'], size)
+        pubkey_bytes = bytes.fromhex(values["-Clave-"])
+        sig_bytes = bytes.fromhex(values["-Firma-"])
+
+        result = sl.schnorr_verify(M, pubkey_bytes, sig_bytes)
+
+        if result:
+            sg.Popup("The signature is VALID for this message and this public key")
+        else:
+            sg.Popup("The signature is NOT VALID for this message and this public key")
+
+
+
 
 window.close()
+ 
